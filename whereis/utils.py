15fc322813e80f6118b585b36ade073e6c70ca40
ALinuxPerson <micheal02052007@gmail.com>
2020-07-06 21:44:00 +0800

    Add absolute_listdir

---
+++ whereis/utils.py
@@ -0,0 +1,21 @@
+# where-is: Finds config files.
+# Copyright (C) 2020 ALinuxPerson
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+import os
+from typing import List
+
+
+def absolute_listdir(directory: str = ".") -> List[str]:
+    return [os.path.join(directory, path) for path in os.listdir(directory)]
