f2ee26e65240103ec5cbbf9f0a4dc8b0beb4fbc9
ALinuxPerson <micheal02052007@gmail.com>
2020-07-09 19:54:28 +0800

    Add base for cli.py

---
+++ whereis/cli.py
@@ -0,0 +1,30 @@
+# where-is: Finds config files.
+# Copyright (C) 2020 ALinuxPerson
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+from argparse import ArgumentParser, Namespace
+from typing import List
+import sys
+
+
+def parser() -> ArgumentParser:
+    pass
+
+
+def parse_args(argparser: ArgumentParser, args: List[str] = None) -> Namespace:
+    args = args or sys.argv[1:]
+
+
+def main(args: List[str] = None) -> None:
+    args = args or sys.argv[1:]
