3efdded343a30c5f82688a34d63d514120c5cbb0
ALinuxPerson <micheal02052007@gmail.com>
2020-07-10 20:56:58 +0800

    Add 'add' method for entry

--- whereis/clifire.py
+++ whereis/clifire.py
@@ -13,7 +13,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-from whereis import Database
+from whereis import Database, Entry, levels, exceptions
 import fire
 from typing import List
 
@@ -22,8 +22,24 @@ class EntryCLI:
     def __init__(self) -> None:
         self._database: Database = Database()
 
-    def add(self, name: str, *locations: List[str]) -> None:
+    @staticmethod
+    def _comma_delimit_to_list(comma_delimit: str) -> List[str]:
+        """Converts a comma-delimited string to a list fit for being passed to Entry.*locations.
+
+        Args:
+            comma_delimit: The comma-delimited string.
+
+        Returns:
+            A converted comma-delimited string.
+        """
+        return comma_delimit.split(",")
+
+    def add(self, name: str, *locations: str) -> None:
         """Adds an entry to the database.
+
+        Notes:
+            Note that you SHOULD NOT use this method to add entries to the database. You should use the better method,
+            create the entry from scratch using a JSON.
 
         Args:
             name: The name of the entry to add.
@@ -32,7 +48,17 @@ class EntryCLI:
         Returns:
             Nothing.
         """
-        print(name, locations)
+        converted_locations: List[List[str]] = [
+            self._comma_delimit_to_list(location) for location in locations
+        ]
+        entry: Entry = Entry(name, *converted_locations)
+        entry.database = self._database
+        try:
+            entry.add()
+        except exceptions.EntryExistsError as error:
+            levels.error(f"The specified entry exists: {error}")
+            return
+        levels.success(f"Successfully added entry to database.")
 
 
 class FindCLI:
