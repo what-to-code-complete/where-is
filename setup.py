901f8d56eff83bfc47a6203c7233f3bd9c77971e
ALinuxPerson <micheal02052007@gmail.com>
2020-07-11 20:55:08 +0800

    Add required, extras, license, classifiers for setup.py and update long description for README.md

--- setup.py
+++ setup.py
@@ -13,19 +13,29 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+from typing import List, Dict
 import setuptools
 import pathlib
+
+REQUIRED: List[str] = ["rich==3.1.0", "fire==0.3.1"]
+EXTRAS: Dict[str, List[str]] = {"dev": ["mypy==0.782"]}
 
 setuptools.setup(
     name="where-is",
     version=pathlib.Path("VERSION").read_text().strip(),
     description="Finds config files.",
-    long_description=pathlib.Path("README.rst").read_text().strip(),
+    long_description=pathlib.Path("README.md").read_text().strip(),
     author="ALinuxPerson",
     author_email="micheal02052007@gmail.com",
     python_requires=">=3.6.0",
     packages=setuptools.find_packages(
         exclude=["tests", "*.tests", "*.tests.*", "tests.*"]
     ),
-    entry_points={"console_scripts": ["where-is=whereis.__main__:main"],},
+    entry_points={"console_scripts": ["where-is=whereis.__main__:main"]},
+    include_package_data=True,
+    license="GNU GPLv3",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+    ],
 )
