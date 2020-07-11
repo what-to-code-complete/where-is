562742f45355bf990e6f4e7caa1ede539be16e23
ALinuxPerson <micheal02052007@gmail.com>
2020-07-11 15:02:29 +0800

    Remove clifire.py to cli.py

--- whereis/__main__.py
+++ whereis/__main__.py
@@ -13,7 +13,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-from whereis.clifire import main as cli_main
+from whereis.cli import main as cli_main
 
 
 def main() -> None:
