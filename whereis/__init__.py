ad1b20d6a0776bb0189f4d9852b3973aba797dab
ALinuxPerson <micheal02052007@gmail.com>
2020-07-11 15:01:46 +0800

    Remove cli.py

--- whereis/__init__.py
+++ whereis/__init__.py
@@ -14,4 +14,4 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 from whereis.database import *
-from whereis import __main__, cli, clifire
+from whereis import __main__, clifire
